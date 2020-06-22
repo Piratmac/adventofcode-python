085b375c30268fb1cdc9012157aa405689b5e67b
Piratmac <6253139+Piratmac@users.noreply.github.com>
2020-06-22 21:26:16 +0200

    Racetrack: moved some elements to complex utils library

--- 2018/racetrack.py
+++ 2018/racetrack.py
@@ -1,58 +1,8 @@
 from math import sqrt
 
-# Cardinal directions
-north = 1j
-south = -1j
-west = -1
-east = 1
-directions_all = [north, south, west, east]
-directions_horizontal = [west, east]
-directions_vertical = [north, south]
-
-# To be multiplied by the current cartinal direction
-relative_directions = {
-    "left": 1j,
-    "right": -1j,
-    "ahead": 1,
-    "back": -1,
-}
-
 
 class PlayerBlocked(Exception):
     pass
-
-
-def min_real(complexes):
-    real_values = [x.real for x in complexes]
-    return min(real_values)
-
-
-def min_imag(complexes):
-    real_values = [x.imag for x in complexes]
-    return min(real_values)
-
-
-def max_real(complexes):
-    real_values = [x.real for x in complexes]
-    return max(real_values)
-
-
-def max_imag(complexes):
-    real_values = [x.imag for x in complexes]
-    return max(real_values)
-
-
-def complex_sort(complexes, mode=""):
-    # Sorts by real, then by imaginary component (x then y)
-    if mode == "xy":
-        complexes.sort(key=lambda a: (a.real, a.imag))
-    # Sorts by imaginary, then by real component (y then x)
-    elif mode == "yx":
-        complexes.sort(key=lambda a: (a.imag, a.real))
-    # Sorts by distance from 0,0 (kind of polar coordinates)
-    else:
-        complexes.sort(key=lambda a: sqrt(a.imag ** 2 + a.real ** 2))
-    return complexes
 
 
 def collisions(players):
